d558623cfa8a5eb71981b9004fe3ceed01243d79
Eric <eric@baochi.xin>
2023-06-26 12:46:28 +0800

    upgrade describetion

--- example/describe.ts
+++ example/describe.ts
@@ -16,14 +16,10 @@ async function main() {
     Ws: true,
   });
   await client.Connect();
-  const msg = await client.Describe(
+  const Describe = await client.Describe(
     "https://cdn.discordapp.com/attachments/1107965981839605792/1119977411631652914/Soga_a_cool_cat_blue_ears_yellow_hat_02afd1ed-17eb-4a61-9101-7a99b105e4cc.png"
   );
-  // const fileinfo = await client.MJApi.UploadImage('https://img.ohdat.io/midjourney-image/1b74cab8-70c9-474e-bfbb-093e9a3cfd5c/0_1.png')
-  // console.log(fileinfo)
-  // const msg = await client.Describe(fileinfo)
-
-  console.log({ msg });
+  console.log(Describe);
   client.Close();
 }
 main().catch((err) => {
